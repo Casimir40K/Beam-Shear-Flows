0284bd3137b1cd091160519527a59fe87bb5a3a9
Casimir40K <casimir.solomon@gmail.com>
2025-08-11 20:10:18 +0100

    added Boom areas function to compute boom areas

---
+++ BoomAreas.m
@@ -0,0 +1,21 @@
+function B = BoomAreas(nodes, connections)
+
+nNodes = length(nodes);
+B = zeros(nNodes,1);
+
+for i = 1:nNodes
+    attachedNodes = (connections(1,:)==i); %Finds the nodes attached to the current boom anf gives a logical response
+    trimmedConnections = connections(1:2,:); % Cuts of the thickness row
+    trimmedConnections = find(attachedNodes)'; % Gives the collumn numbers of the attached booms
+    nConnections = length(trimmedConnections); % number of connections for loop purposes
+    currentNodeLocation = nodes(i,:); % Coordinate location of the current boom
+    for j = 1:nConnections
+        connectionIndex = trimmedConnections(j);
+        connectedNodeIndex = connections(2,connectionIndex);
+        connectedNodeLocation = nodes(connectedNodeIndex,:);
+        l = norm(currentNodeLocation - connectedNodeLocation);
+        t = connections(3,connectionIndex);
+        sigma = connectedNodeLocation(2)/currentNodeLocation(2);
+        B(i) = B(i) + ((l*t)/6)* (2 + sigma);
+    end
+end
